a901aec52ae91c3cddcbf191ee12befe7ff81f90
najam sikander awan <najamsk@gmail.com>
2014-10-01 09:38:26 +0500

    email from powershell using smtp

---
+++ GmailSmtp.ps1
@@ -0,0 +1,9 @@
+﻿$EmailFrom = "from@mail.com"
+$EmailTo = "to@mail.com" 
+$Subject = "emailing powershell report" 
+$Body = "this should work on remove server as well. test email" 
+$SMTPServer = "smtp.gmail.com" 
+$SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587) 
+$SMTPClient.EnableSsl = $true 
+$SMTPClient.Credentials = New-Object System.Net.NetworkCredential("gmailUser", "gmailPasswor"); 
+$SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)
